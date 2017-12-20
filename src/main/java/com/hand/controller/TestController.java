ade04473b4569f00d384b5d15de13c22b3adbd3d
engikeer <engikeer@163.com>
2017-12-21 03:08:04 +0800

    Update TestController.java

--- src/main/java/com/hand/controller/TestController.java
+++ src/main/java/com/hand/controller/TestController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class TestController {
 
-    // 测试
     @RequestMapping(value = "/admin")
     @ResponseBody
     public String testSpring() {
