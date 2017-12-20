32a819cedc95290e13e355644d1f5c6d640e5fdd
engikeer <engikeer@163.com>
2017-12-20 15:13:43 +0800

    update test

--- src/main/java/com/hand/controller/TestController.java
+++ src/main/java/com/hand/controller/TestController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class TestController {
+
+//    测试函数
     @RequestMapping(value = "/admin")
     @ResponseBody
     public String testSpring() {
