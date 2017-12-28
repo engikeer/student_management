72ea8b1d808cda42172de49a6163fce31adc9525
engikeer <engikeer@163.com>
2017-12-28 20:34:41 +0800

    update sth

--- src/main/java/com/hand/home/dao/StudentDaoImpl.java
+++ src/main/java/com/hand/home/dao/StudentDaoImpl.java
@@ -1,35 +1,29 @@
 package com.hand.home.dao;
 
 import com.hand.home.pojo.Student;
-import com.hand.utils.DBConnect;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.stereotype.Repository;
 
-import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
 
 @Repository
 public class StudentDaoImpl implements StudentDao {
-    private DBConnect dbConect;
+    private JdbcTemplate jdbcTemplate;
 
     @Autowired
-    public StudentDaoImpl(DBConnect dbConnect) {
-        this.dbConect = dbConnect;
+    public StudentDaoImpl(DriverManagerDataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
     @Override
     public boolean insert(Object obj) {
-        try{
             Student student = (Student) obj;
             String sqlInsert = "INSERT INTO student (name, age) VALUES (?, ?)";
-            dbConect.update(sqlInsert, student.getName(), student.getAge());
+            jdbcTemplate.update(sqlInsert, student.getName(), student.getAge());
             return true;
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return false;
-        }
-
     }
 
     // TODO 干点啥
